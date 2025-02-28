9fe23cf0d7a46996f5db7d87582e5703aff755a9
Tehila Kersh <Tehila.kersh@gmail.com>
2025-02-28 02:30:31 +0200

    Fix port binding

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import json
+import os
 
 app = Flask(__name__)
 
@@ -182,4 +183,5 @@ def deposit(account_number):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    #app.run(debug=True)
